283ffeb5b406ec5fe84dace9f8c44223992396b3
Martim Gil <a.martimgil@gmail.com>
2026-03-02 18:42:25 +0000

    feat: Introduce NotificationContext for centralized notification management and integrate it into the application.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,16 @@ import { router } from './routes';
 import { Toaster } from './components/ui/sonner';
 import { ThemeProvider } from './context/ThemeContext';
 import { UserProvider } from './context/UserContext';
+import { NotificationProvider } from './context/NotificationContext';
 
 export default function App() {
   return (
     <ThemeProvider>
       <UserProvider>
-        <RouterProvider router={router} />
-        <Toaster position="top-right" richColors closeButton />
+        <NotificationProvider>
+          <RouterProvider router={router} />
+          <Toaster position="top-right" richColors closeButton />
+        </NotificationProvider>
       </UserProvider>
     </ThemeProvider>
   );
